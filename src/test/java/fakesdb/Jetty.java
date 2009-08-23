c8e55bd74f7b1547949979078ba7dc759578f698
Stephen Haberman <stephen@exigencecorp.com>
2009-08-22 21:37:59 -0500

    Use two space indent for the Jetty.java file too.

--- src/test/java/fakesdb/Jetty.java
+++ src/test/java/fakesdb/Jetty.java
@@ -7,29 +7,27 @@ import org.mortbay.jetty.servlet.ServletHandler;
 
 public class Jetty {
 
-	private static final Server SERVER = new Server();
+  private static final Server SERVER = new Server();
 
-	public static void main(String[] args) {
-		// Use SocketConnector because SelectChannelConnector locks files
-		Connector connector = new SocketConnector();
-		connector
-				.setPort(new Integer(System.getProperty("jetty.port", "8080")));
-		connector.setMaxIdleTime(60000);
+  public static void main(String[] args) {
+    // Use SocketConnector because SelectChannelConnector locks files
+    Connector connector = new SocketConnector();
+    connector.setPort(new Integer(System.getProperty("port", "8080")));
+    connector.setMaxIdleTime(60000);
 
-		ServletHandler handler = new ServletHandler();
-		handler.addServletWithMapping(FakeSdbServlet.class.getName(), "/*");
+    ServletHandler handler = new ServletHandler();
+    handler.addServletWithMapping(FakeSdbServlet.class.getName(), "/*");
 
-		Jetty.SERVER.setConnectors(new Connector[] { connector });
-		Jetty.SERVER.setHandlers(new Handler[] { handler });
-		Jetty.SERVER.setAttribute(
-				"org.mortbay.jetty.Request.maxFormContentSize", 0);
-		Jetty.SERVER.setStopAtShutdown(true);
+    Jetty.SERVER.setConnectors(new Connector[] { connector });
+    Jetty.SERVER.setHandlers(new Handler[] { handler });
+    Jetty.SERVER.setAttribute("org.mortbay.jetty.Request.maxFormContentSize", 0);
+    Jetty.SERVER.setStopAtShutdown(true);
 
-		try {
-			Jetty.SERVER.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    try {
+      Jetty.SERVER.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 }
