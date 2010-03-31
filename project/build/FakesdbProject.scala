205a60ff39e77723c618a6d51df418ec9b2a7a99
Stephen Haberman <stephen@exigencecorp.com>
2010-03-31 13:17:33 -0500

    Add servletapi as a buildtime conf and put it on the compileClasspath.

--- project/build/FakesdbProject.scala
+++ project/build/FakesdbProject.scala
@@ -1,4 +1,5 @@
 import sbt._
 
 class TestProject(info: ProjectInfo) extends DefaultWebProject(info) {
+  override def compileClasspath = super.compileClasspath +++ managedClasspath(config("buildtime"))
 }
