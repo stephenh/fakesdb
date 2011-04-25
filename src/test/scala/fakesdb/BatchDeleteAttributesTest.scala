cd239b17cecefb591ea5184185763dacd3d1b0e6
Stephen Haberman <stephen@exigencecorp.com>
2011-04-25 14:16:22 -0500

    We can test BatchDeleteAttributes now with the AWS SDK.

--- src/test/scala/fakesdb/BatchDeleteAttributesTest.scala
+++ src/test/scala/fakesdb/BatchDeleteAttributesTest.scala
@@ -2,39 +2,37 @@ package fakesdb
 
 import org.junit._
 import org.junit.Assert._
+import com.amazonaws.services.simpledb.model._
 
 class BatchDeleteAttributesTest extends AbstractFakeSdbTest {
 
   @Before
   def createDomain(): Unit = {
-    createDomain("domaina")
+    createDomain(domaina)
   }
 
-  //@Test
-  //def testDelete(): Unit = {
-    //add(domaina, "itema", "a" -> "1")
-    //add(domaina, "itemb", "a" -> "1", "b" -> "1")
+  @Test
+  def testDelete(): Unit = {
+    add(domaina, "itema", "a" -> "1")
+    add(domaina, "itemb", "a" -> "1", "b" -> "1")
+    
+    sdb.batchDeleteAttributes(new BatchDeleteAttributesRequest().withDomainName(domaina).withItems(
+      new DeletableItem().withName("itema"),
+      new DeletableItem().withName("itemb")
+    ))
 
-    //val as = new java.util.ArrayList[ItemAttribute]()
-    //val bs = new java.util.ArrayList[ItemAttribute]()
-    //val m = new java.util.HashMap[String, java.util.List[ItemAttribute]]()
-    //m.put("itema", as)
-    //m.put("itemb", bs)
-    //domaina.batchDeleteAttributes(m)
+    assertEquals(0, select("SELECT * FROM domaina").getItems.size)
+  }
 
-    //assertEquals(0, domaina.selectItems("SELECT * FROM domaina", null, true).getItems.size)
-  //}
-
-  //@Test
-  //def testDeleteTooMany(): Unit = {
-    //val m = new java.util.HashMap[String, java.util.List[ItemAttribute]]()
-    //for (i <- 1.to(26)) {
-      //val as = new java.util.ArrayList[ItemAttribute]()
-      //m.put("item" + i, as)
-    //}
-    //assertFails("NumberSubmittedItemsExceeded", "Too many items in a single call. Up to 25 items per call allowed.", {
-      //domaina.batchDeleteAttributes(m)
-    //})
-  //}
+  @Test
+  def testDeleteTooMany(): Unit = {
+    val req = new BatchDeleteAttributesRequest().withDomainName(domaina)
+    for (i <- 1.to(26)) {
+      req.withItems(new DeletableItem().withName("item" + i))
+    }
+    assertFails("NumberSubmittedItemsExceeded", "Too many items in a single call. Up to 25 items per call allowed.", {
+      sdb.batchDeleteAttributes(req)
+    })
+  }
 
 }
